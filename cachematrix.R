ece03f1912ecc520eb8227190c528efc75b2b9dd
Sam-jevi <fabulasamantha@gmail.com>
2021-04-21 00:43:26 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There is an object called special 
+## matrix that can inverse the cache.
+## In this computation it is all
+## about the Matrix inversion which
+## could result to a high cost.
+## It also utilizes the computation
+## of the inverse matrix over and over.
+## There are also usage on creating
+## special object of pairing the functions
+## that stores inverse matrix and caches.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() {x}
+  setInverse <- function(inverse) {inv <<- inverse}
+  getInverse <- function() {inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
+## After the special matrix, there should
+## be another important function named 
+## cacheSolve where it computes the 
+## recurred feedback of the makeCacheMatrix.
+## In recovering the cache inverse it 
+## needs to be the same as it is in 
+## examining the special matrix
+## that is inverse and computed instrument.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cache data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
